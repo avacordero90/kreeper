cc7ed7d1c89122470d822687651c47f48046a47a
Ava Cordero <avacordero90@gmail.com>
2021-11-17 23:13:54 -0500

    Update mysteries URL and port

--- kreeper.py
+++ kreeper.py
@@ -139,7 +139,7 @@ def run_kreeper ():
             
             # analyze each table to determine action
             # url = 'https://api.kreeper.trade/mysteries/analyze'
-            url = 'https://35.247.36.101/mysteries/analyze'
+            url = 'https://api.kreeper.trade:444/mysteries/analyze'
             payload = {'pair': pair, 'table': markets[pair], 'balances': balances}
             data = requests.get(url, data = payload)
             
