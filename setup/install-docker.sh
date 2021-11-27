f7672873d450d18af9d30542b270427cd106d24a
Ava Cordero <avacordero90@gmail.com>
2021-11-27 13:29:42 -0500

    create install-docker.sh

---
+++ setup/install-docker.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+echo "initializing kreeper server installation ..."
+
+sleep 1
+
+echo "========================================================"
+echo "this installer is for internal use only."
+echo "requires debian-based linux and root access."
+echo "recommended to install to a vm or docker container."
+echo "========================================================"
+
+echo "continue? yes/no"
+read answer
+
+if [[ $answer == 'y'* ]]; then
+    cd ~
+    rm -rf ~/kreeper/ /kreeper/
+
+    apt update && \
+        apt install -y curl git
+
+    git clone git@github.com:avacordero90/kreeper.git
+
+    # cd ~/kreeper
+
+    echo -e "\n" | bash <(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
+
+    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.profile
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+    apt install python=3.10
+    
+    # export PATH=$PATH:/home/linuxbrew/.linuxbrew/opt/python@3.10/bin
+    # source ~/.profile
+
+    # pip3 install pipenv
+
+    # rm -f ~/kreeper/Pipfile
+
+    # pipenv clean
+
+    # ln -s ~/kreeper/kreeper.py /home/linuxbrew/.linuxbrew/bin/kreeper.py --force
+    # chmod u+x /home/linuxbrew/.linuxbrew/bin/kreeper.py
+    # source ~/.profile
+
+    # pipenv shell source ~/kreeper/setup/config.sh
+    source ~/kreeper/setup/config.sh
+
+    # if [[ $1 ]]; then
+        # pipenv --version
+
+        if [[ $1 ]]; then
+            echo -e "installation complete!\n"
+        else
+            echo -e "installation failed: unknown error.\n"
+        fi
+    # else
+    #     echo -e "installation failed!\n"
+    # fi
+
+else
+    echo -e "installation aborted!\n"
+fi
+
+
