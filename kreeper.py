539e0d9dc04f87e3aa0e6d85fb07570487cae54d
Ava Cordero <avacordero90@gmail.com>
2021-11-28 21:27:50 -0500

    Testing

--- kreeper.py
+++ kreeper.py
@@ -47,7 +47,7 @@ from source.server import start_server
 VERSION = "1.0.8"
 
 
-context = SSL.Context(SSL.OP_NO_TLSv1_2)
+context = SSL.Context(SSL.TLS1_2_VERSION)
 context.use_privatekey_file('ssl/privkey.pem')
 context.use_certificate_file('ssl/62ba54b69c53d5bf.pem')
 
