9ca4047174412830d13eda1d3798b704b13d1f7a
Ava Cordero <avacordero90@gmail.com>
2021-12-05 03:06:23 -0800

    Fixed start.sh and docker-start.sh

--- setup/docker-start.sh
+++ setup/docker-start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo -e "Enter your PEM passphrase: "
+read -s ssl_pp
+
 sudo docker build -t kreeper ~/kreeper && \
     sudo docker run -p 0.0.0.0:443:443/tcp -e SSL_PASSPHRASE=$ssl_pp -it kreeper
--- start.sh
+++ start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo -e "Enter your PEM passphrase: "
-read -s ssl_pp
-
 ~/kreeper/setup/docker-cleanup.sh
 ~/kreeper/setup/docker-config-dev.sh
 ~/kreeper/setup/docker-start.sh
